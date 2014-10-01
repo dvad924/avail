8c07f0759676527c4831f6f959fd79396408d0a0
Ryan <r.k.dubowsky@gmail.com>
2014-10-01 13:02:25 -0400

    done with basic visualization and table

--- nycountiesproj/helpers.js
+++ nycountiesproj/helpers.js
@@ -11,10 +11,10 @@ function drawTable(DataObj){
     DataMatrix.push(listcopy);
   }
   //Finish handling input Data
-  var body = d3.select("body");             //Get the body  
-
+  var body = d3.select("body")             //Get the body  
+                .style("background-color","#009999");
   var div = body.append("div").style("overflow-y","auto") //Create Main Div for data display
-                              .style({"height":"300px","display":"inline-block"}); //make scrollable if long
+                              .style({"height":"230px","display":"inline-block"}); //make scrollable if long
   var table = div.append("table");
       table.attr("id","dataTable"); //create table inside our div//
 
@@ -28,7 +28,7 @@ function drawTable(DataObj){
 
   var td = tr.selectAll("td").data(function(d){return d}).enter() //for each datum in the row
               .append("td").text(function(datum){return datum});
-  console.log(td);
+  
   var thead = table.append("thead")
   var trhead  = thead.append("tr");
       trhead.selectAll("td").data(columns).enter()
@@ -36,7 +36,33 @@ function drawTable(DataObj){
         .text(function(d){return d});
 }
 
+function updateTable(DataObj,type){
 
+  var columns = censusVars;
+  columns.unshift("id");
+  var DataMatrix = [];
+  for( var property in DataObj){
+    listcopy = DataObj[property].slice();
+    for( var i=0; i < listcopy.length; i++){
+
+      listcopy[i] = (type == 1)? dataMapping(type,DataObj,property,i).toFixed(5)
+                                :dataMapping(type,DataObj,property,i);
+    }
+    listcopy.unshift(property)
+    DataMatrix.push(listcopy);
+  }
+
+  d3.selectAll("#dataTable tr").data(DataMatrix).selectAll("td")
+                              .data(function(d){return d})
+                              .text(function(datum){return datum});
+}
+
+var dataMapping = function(type,data,key,index){
+    if(type === 0)
+        return 1*data[key][index];
+    if(type===1)
+        return 1*(data[key][index])/(data[key][totalOccupancy]);
+}
 
 var mapping =  function(type,data,key){
     if(type === 0)
@@ -47,14 +73,23 @@ var mapping =  function(type,data,key){
 
 
 
-//Census data must be defined befor use;
+//Census data must be defined before use;
 var getDomain = function(){
   var domain = [];
   for(key in censusData){
     domain.push( mapping(maptype,censusData,key) );
   }
   
-  domain.sort();
+  domain.sort(function(a,b){return a-b});
+  var index=0;
+  for(var i in domain){
+    if(domain[i] == 0)
+      index = i;
+    else
+      break;
+  }//Here we want to eliminate zeros from destroying our color scheme 
+   //So we remove them from the domain being mapped to colors
+
   return domain;
 }
  
@@ -85,7 +120,7 @@ var getDomain = function(){
     if(maptype ===1){
      for(var i = 0; i < list.length; i++){ 
         if(i ==0)
-          labelList.push("x < " + (100*list[i+1]).toFixed(3) +"%");
+          labelList.push("x < " + (100*list[i]).toFixed(3) +"%");
         if(i == list.length-1)
          labelList.push("x > " + (100*list[i]).toFixed(3) + "%");
         else
